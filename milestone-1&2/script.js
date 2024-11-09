8f18a00e234f644293b02e458d28ff67d7e9e62b
unknown <abdullahrustam12345@gmail.com>
2024-11-10 01:34:40 +0500

    first commitUpdate

---
+++ milestone-1&2/script.js
@@ -0,0 +1,8 @@
+"use strict";
+const toggleBtn = document.getElementById("toggle-btn");
+const toggle = document.getElementById("toggle");
+function togglediv() {
+    toggle.style.display = toggle.style.display === "none" ? "block" : "none";
+    toggleBtn.textContent = toggleBtn.textContent === "arrow_downward" ? "arrow_upward" : "arrow_downward";
+}
+toggleBtn.addEventListener("click", togglediv);
---
+++ milestone-3/script.js
@@ -0,0 +1,149 @@
+"use strict";
+const NameEle = document.getElementById("First-name");
+const emailEle = document.getElementById("Email");
+const numberEle = document.getElementById("Number");
+const pictureEle = document.getElementById("Picture");
+const educationYearEle = document.getElementById("Year");
+const educationEle = document.getElementById("Edu");
+const educationInstitutEle = document.getElementById("Ins");
+const experienceEle = document.getElementById("Exp");
+const experienceDurationEle = document.getElementById("Dueration");
+const experienceTitleEle = document.getElementById("Title");
+const skillEle = document.getElementById("Skills");
+const output = document.getElementById("resumeOutput");
+const submitBtn = document.getElementById("sub-btn");
+const aboutEle = document.getElementById("About");
+const fieldEle = document.getElementById("your-field");
+let img = "";
+function handleImageUpload() {
+    if (pictureEle) {
+        const file = pictureEle.files[0];
+        const reader = new FileReader();
+        reader.addEventListener("load", () => {
+            img = reader.result;
+        });
+        reader.addEventListener("error", () => {
+            console.error("Error reading file");
+        });
+        reader.readAsDataURL(file);
+    }
+}
+function generateResume() {
+    if (NameEle &&
+        emailEle &&
+        numberEle &&
+        pictureEle &&
+        educationEle &&
+        fieldEle &&
+        educationInstitutEle &&
+        educationYearEle &&
+        experienceEle &&
+        skillEle &&
+        experienceDurationEle &&
+        experienceTitleEle) {
+        const userDetails = {
+            name: NameEle.value,
+            number: numberEle.value,
+            email: emailEle.value,
+            picture: pictureEle.value,
+            education: educationEle.value,
+            filed: fieldEle.value,
+            educationIns: educationInstitutEle.value,
+            educationYear: educationYearEle.value,
+            experience: experienceEle.value,
+            experienceDur: experienceDurationEle.value,
+            experienceTitle: experienceTitleEle.value,
+            skill: skillEle.value,
+            About: aboutEle.value,
+        };
+        console.log(userDetails.About);
+        let resume = `  <main>
+      <div class="Box"  contenteditable="true">
+        <div class="Information">
+          <div class="Pic">
+            <img
+              class="profile-img"
+              src=${img}
+              alt="Img"
+              height="100%"
+              width="50%"
+            />
+            <div>
+              <h3>${userDetails.name}</h3>
+              <h5>${userDetails.filed}</h5>
+            </div>
+          </div>
+          <div class="contact">
+            <h3>Contact :</h3>
+            <div class="flex">
+              <img src="./img/Email Icon.png" alt="" />
+              <p>${userDetails.email}</p>
+            </div>
+            <div class="flex">
+              <img src="./img/Phone Number.png" alt="" />
+              <p>${userDetails.number}</p>
+            </div>
+          </div>
+        </div>
+        <div class="about">
+          <h3>About</h3>
+          <p>
+            ${userDetails.About}
+          </p>
+        </div>
+        <div class="Work">
+          <h3>Work Experience</h3>
+          <div class="work-flex">
+            
+              <div class="work-Ex">
+                <h4>${userDetails.experienceTitle}</h4>
+                <p>2${userDetails.experienceDur}</p>
+              </div>
+              <p
+                >${userDetails.experience}</p
+              >
+            
+          </div>
+        </div>
+        <div class="education">
+          <h3>Education</h3>
+          <div class="edu-flex">
+            <div class="education-Ex">
+              <p>${userDetails.educationYear}</p>
+              <h4>${userDetails.education}</h4>
+              <p id="p">${userDetails.educationIns}</p>
+            </div>
+          </div>
+        </div>
+        <div class="Skill">
+          <div class="skill-ex">
+          <h3>Skills</h3>
+          </div>
+          <div id="toggle">
+            <ul>
+              <li>${userDetails.skill}</li>
+            </ul>
+          </div>
+        </div>
+      </div>`;
+        if (userDetails.name !== "" &&
+            userDetails.number !== "" &&
+            userDetails.email !== "" &&
+            userDetails.education !== "" &&
+            userDetails.educationIns !== "" &&
+            userDetails.educationYear !== "" &&
+            userDetails.experience !== "" &&
+            userDetails.experienceDur !== "" &&
+            userDetails.experienceTitle !== "" &&
+            img !== "" &&
+            userDetails.skill !== "") {
+            output.innerHTML = resume;
+        }
+        else {
+            alert("Fill Complete Information");
+        }
+    }
+}
+;
+pictureEle.addEventListener("change", handleImageUpload);
+submitBtn.addEventListener("click", generateResume);
